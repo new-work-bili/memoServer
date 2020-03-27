f070c4f19ae7e9cc562371e73348b67e6f8f6b4a
LPP <1655170926@qq.com>
2020-03-27 11:43:22 +0800

    whit mingdan3

--- routes/QQ.js
+++ routes/QQ.js
@@ -6,3 +6,5 @@ const {login,init,add,edit,delet} = require('../db/sql.js')
 router.get('/memo/qqlogin/', function(req, res, next) {
 	console.log('req.body:',req.body)
 });
+
+module.exports = router;
