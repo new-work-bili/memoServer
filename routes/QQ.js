d05c061951155112ff8d71abd8eb363a1ac659fe
LPP <1655170926@qq.com>
2020-03-27 15:17:32 +0800

    whit mingdan3

--- routes/QQ.js
+++ routes/QQ.js
@@ -7,8 +7,8 @@ router.get('/memo/qqlogin/', function(req, res, next) {
 	console.log('req.query:',req.query)
 	console.log('QQreq.url',req.url)
 	const code = req.query.code
-	const appId = 101851279
-	const appKey = 0835ace1f5216c95aa1fb5930168f50a
+	const appId = '101851279'
+	const appKey = '0835ace1f5216c95aa1fb5930168f50a'
 	const redirect_uri = 'http://lppwork.cn/memo/qqlogin'
 	res.redirect(`https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=${appId}&client_secret=${appKey}&code=${code}&redirect_uri=${redirect_uri}`);
 	// res.json({
