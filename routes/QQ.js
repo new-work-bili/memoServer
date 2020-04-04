23d018a21e30078fbc4f3ab00a30f7328b6c204f
LPP <1655170926@qq.com>
2020-04-04 22:46:15 +0800

    qqlogin

--- routes/QQ.js
+++ routes/QQ.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const request = require("request");
 const jwt = require('jsonwebtoken')
-const {PRIVATE_KEY,tokenOutTime} = require('../config.js')
+const {
+	PRIVATE_KEY,
+	tokenOutTime
+} = require('../config.js')
 const {
 	login,
 	init,
@@ -15,14 +18,13 @@ const {
 const appId = '101851279'
 const appKey = '0835ace1f5216c95aa1fb5930168f50a'
 var redirect_uri = 'http://lppwork.cn/memo/qqlogin' //回调域
-var userData = {	//返回用户数据
-	account:'',
-	password:'',
+var userData = { //返回用户数据
+	account: '',
+	password: '',
 }
 
 router.get('/memo/qqlogin/', function(req, res, next) {
 	console.log('req.query:', req.query)
-	console.log('req：', req)
 	var code = req.query.code //获取code
 	//通过code获取token
 	var getTokenUrl =
@@ -53,40 +55,42 @@ router.get('/memo/qqlogin/', function(req, res, next) {
 			}, (err, httpResponse, body) => {
 				console.log('get用户数据')
 				body = JSON.parse(body);
-				console.log('body:',body)
+				console.log('body:', body)
 				userData.account = body.nickname
-				userData.password = qqOpenid	//把Openid这个唯一标识作为密码
+				userData.password = qqOpenid //把Openid这个唯一标识作为密码
 				//把登陆信息放入数据库
 				login(userData).then(Lbdres => {
 					if (!Lbdres || Lbdres.length == 0) {
 						//未注册，进行注册
-						registe(userData).then((Rbdres)=>{
+						registe(userData).then((Rbdres) => {
 							console.log('QQ注册成功!')
-						}).catch((err)=>{
-							console.log('QQ注册失败!',err)
-						})
-					} else {
-						var username = userData.account
-						const token = jwt.sign( 		//生成token
-							{username},
-							PRIVATE_KEY, 
-							{expiresIn: tokenOutTime}
-						)
-						console.log('QQ生成token成功')
-						res.json({
-							userData:userData,
-							token:token
+						}).catch((err) => {
+							console.log('QQ注册失败!', err)
 						})
 					}
-				
+					var username = userData.account
+					const token = jwt.sign( //生成token
+						{
+							username
+						},
+						PRIVATE_KEY, {
+							expiresIn: tokenOutTime
+						}
+					)
+					console.log('QQ生成token成功')
+					res.json({
+						userData: userData,
+						token: token
+					})
+
 				}).catch((err) => {
 					console.log('QQ登陆err:', err)
 				})
-				
+
 			})
 		})
 	})
-	
+
 });
 
 module.exports = router;
