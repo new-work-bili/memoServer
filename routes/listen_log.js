fc7b8b59be3b6251eaeef56bc9d09a4b6699bbbb
LPP <1655170926@qq.com>
2020-10-10 23:53:10 +0800

    debug

--- routes/listen_log.js
+++ routes/listen_log.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 
 
 
-router.get('/ListenLog/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	var pares = req.url.split('?')[1]
 	var data = qs.parse(pares)
 	res.status(204); //无需返回,204
